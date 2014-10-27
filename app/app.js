68d5b392570d2e2f663171ed186ea79da80e772b
Francis Fernandes da Luz <francisfl@ciandt.com>
2014-10-27 18:57:41 -0200

    V0 GDrive e Ajustes de Layout

--- app/app.js
+++ app/app.js
@@ -1,6 +1,19 @@
-var app = angular.module("ChatApp", ["ngMaterial", "firebase"]);
+var app = angular.module("ChatApp", ["ngMaterial", "firebase", "lk-google-picker"])
+.config(['lkGoogleSettingsProvider', function(lkGoogleSettingsProvider) {
+
+  lkGoogleSettingsProvider.configure({
+    apiKey   : 'AIzaSyAnYxzizdvb6TMiIpDgBW_FAXXLeSgDnzU',
+    clientId : '752915435201-qf5shuiaos1rks9k8bcgccadtcuvf4tt.apps.googleusercontent.com',
+    scopes 	 : ['https://www.googleapis.com/auth/plus.login'],
+    locale   : 'pt-br'
+   })
+ }]);
+
 
 app.controller("ChatCtrl", function($scope, $firebase, $firebaseSimpleLogin) {
+
+  $scope.files = [];
+
   var ref = new Firebase("https://fiery-fire-1483.firebaseio.com/messages");
   var sync = $firebase(ref);
 
