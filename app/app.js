ccc95f2eb7e959661043088b3ad31acef9538d7c
francisluz <francisluz@gmail.com>
2014-10-27 01:30:44 -0200

    V0.1 Chat App

--- app/app.js
+++ app/app.js
@@ -1,12 +1,15 @@
-var app = angular.module("ChatApp", ["firebase"]);
+var app = angular.module("ChatApp", ["ngMaterial", "firebase"]);
 
-app.controller("ChatCtrl", function($scope, $firebase) {
+app.controller("ChatCtrl", function($scope, $firebase, $firebaseSimpleLogin) {
   var ref = new Firebase("https://fiery-fire-1483.firebaseio.com/messages");
   var sync = $firebase(ref);
+
+  $scope.auth = $firebaseSimpleLogin(ref);
+
   $scope.messages = sync.$asArray();
 
   $scope.addMessage = function(text) {
     $scope.messages.$add({text: text});
-    newMessageText = "";
+    $scope.newMessageText = "";
   }
 });
